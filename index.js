c7998b5f2dc1280888bbaf2662eefc4caa427c0a
elrrrrrrr <elrrrrrrr@gmail.com>
2014-10-13 11:37:56 +0800

    add user interface

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
 var pathUtil = require('path');
-
 var fs = require('fs-extra');
+var inquirer = require('inquirer');
+var facade = require('commander');
+
+var pkg = require('./package.json');
 // var pokemonList = fs.readdirSync('pokemon');
+
+facade
+  .command('catch')
+  .description('catch a pokemon')
+  .action(handlerCatch);
+facade
+  .command('release')
+  .description('release a pokemon')
+	.action(handlerRelease)
+facade
+	.command('list')
+	.description('list current pokemon')
+	.action(showList)
+
+facade
+ .version(pkg.version)
+ .usage('catch')
+ //.option('-R, --releasePoke', 'release a pokemon', handlerRelease)
+ .parse(process.argv);
+
+function showList(type, cb) {
+	inquirer.prompt([
+	  {
+	    type: "list",
+	    name: "pokemon",
+	    message: "pick a pokemon",
+	    choices: type == 'release' ?
+	    						fs.readdirSync(pathUtil.join(__dirname, 'pokemon')) :
+	    						fs.readdirSync(process.cwd())
+	    						//.concat([ new inquirer.Separator(), 'exit'])
+	  }
+	], function( answers ) {
+			
+			cb && cb( answers.pokemon )
+	    //console.log( JSON.stringify(answers, null, "  ") );
+	  });	
+}
+
+function temp() {
+	console.log('still in building');
+}
 
 //如果不是文件夹 抛出异常
 function catchPoke(name, path) {
@@ -23,8 +67,17 @@ function catchPoke(name, path) {
 	}
 }
 
-function releasePoke(name, path) {
+function handlerRelease() {
+	showList( 'release', releasePoke )
+	
+}
 
+function handlerCatch() {
+	showList( 'catch', catchPoke )
+}
+
+function releasePoke(name, path) {
+	
 	var poke = pathUtil.join(__dirname, 'pokemon/' + name);
 	var exits = fs.existsSync(poke);
 
@@ -44,4 +97,6 @@ function killPoke(name) {
 
 }
 
-catchPoke('t')
+function listPoke(name) {
+
+}
