c0c7d8452f65552b3824dd0020c4bae0a2abb15f
elrrrrrrr <elrrrrrrr@gmail.com>
2014-10-13 19:39:44 +0800

    fix the indent

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ facade
 facade
   .command('release')
   .description('release a pokemon')
-	.action(handlerRelease)
+  .action(handlerRelease)
 facade
-	.command('list')
-	.description('list current pokemon')
-	.action(showList)
+  .command('list')
+  .description('list current pokemon')
+  .action(showList)
 facade
-	.command('kill')
-	.description('kill a pokemon')
-	.action(handlerKill)
+  .command('kill')
+  .description('kill a pokemon')
+  .action(handlerKill)
 
 facade
  .version(pkg.version)
@@ -37,94 +37,94 @@ facade
 function showList(type, cb) {
 
 
-	var isDictionary = function(name) {
-		return fs.lstatSync(name).isDirectory();
-	}
+  var isDictionary = function(name) {
+    return fs.lstatSync(name).isDirectory();
+  }
 
-	inquirer.prompt([
-	  {
-	    type: "list",
-	    name: "pokemon",
-	    message: "pick a pokemon",
-	    choices: type == 'catch' ?
-	    						fs.readdirSync(process.cwd()).filter(isDictionary) :
-	    						fs.readdirSync(pathUtil.join(__dirname, 'pokemon'))
-	    						//.concat([ new inquirer.Separator(), 'exit'])
-	  }
-	], function( answers ) {
-			
-			cb && cb( answers.pokemon )
-	    //console.log( JSON.stringify(answers, null, "  ") );
-	  });	
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "pokemon",
+      message: "pick a pokemon",
+      choices: type == 'catch' ?
+                  fs.readdirSync(process.cwd()).filter(isDictionary) :
+                  fs.readdirSync(pathUtil.join(__dirname, 'pokemon'))
+                  //.concat([ new inquirer.Separator(), 'exit'])
+    }
+  ], function( answers ) {
+      
+      cb && cb( answers.pokemon )
+      //console.log( JSON.stringify(answers, null, "  ") );
+    }); 
 }
 
 function temp() {
-	console.log('still in building');
+  console.log('still in building');
 }
 
 //如果不是文件夹 抛出异常
 function catchPoke(name, path) {
 
-	var wild = pathUtil.join(process.cwd(), name);
-	var nick = pathUtil.join(__dirname, 'pokemon', name);
+  var wild = pathUtil.join(process.cwd(), name);
+  var nick = pathUtil.join(__dirname, 'pokemon', name);
 
-	var exits = fs.existsSync(nick);
+  var exits = fs.existsSync(nick);
 
-	if (!exits) {
-		fs.copy(wild, nick, function(err) {
-			if (err) {
-				console.log('error when copy', err)
-			}
-			console.log('OKK')
-		})
-	} else {
-		console.log('already exits');
-	}
+  if (!exits) {
+    fs.copy(wild, nick, function(err) {
+      if (err) {
+        console.log('error when copy', err)
+      }
+      console.log('OKK')
+    })
+  } else {
+    console.log('already exits');
+  }
 }
 
 function handlerRelease() {
-	showList( 'release', releasePoke )
-	
+  showList( 'release', releasePoke )
+  
 }
 
 function handlerCatch() {
-	showList( 'catch', catchPoke )
+  showList( 'catch', catchPoke )
 }
 
 function releasePoke(name, path) {
-	
-	var poke = pathUtil.join(__dirname, 'pokemon/' + name);
-	var exits = fs.existsSync(poke);
+  
+  var poke = pathUtil.join(__dirname, 'pokemon/' + name);
+  var exits = fs.existsSync(poke);
 
-	if (exits) {
-		fs.copy(poke, pathUtil.join(process.cwd(), name), function(err) {
-			if (err) {
-				console.log('error when copy')
-			}
-			console.log('OKK')
-		})
-	} else {
-		console.log('not exits');
-	}
+  if (exits) {
+    fs.copy(poke, pathUtil.join(process.cwd(), name), function(err) {
+      if (err) {
+        console.log('error when copy')
+      }
+      console.log('OKK')
+    })
+  } else {
+    console.log('not exits');
+  }
 }
 
 function handlerKill() {
-	showList( 'kill', killPoke )
+  showList( 'kill', killPoke )
 }
 
 function killPoke(name) {
-	var poke = pathUtil.join(__dirname, 'pokemon/' + name);
-	var exits = fs.existsSync(poke);
+  var poke = pathUtil.join(__dirname, 'pokemon/' + name);
+  var exits = fs.existsSync(poke);
 
-	if (exits) {
-		fs.remove(poke, function(err){
-		  if (err) return console.error(err);
+  if (exits) {
+    fs.remove(poke, function(err){
+      if (err) return console.error(err);
 
-		  console.log("success!")
-		});
-	} else {
-		console.log('no ' + name);
-	}
+      console.log("success!")
+    });
+  } else {
+    console.log('no ' + name);
+  }
 }
 
 function listPoke(name) {
